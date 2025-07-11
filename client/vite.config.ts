c7976fadbf714622f2b2d354cf4980b267f3b065
Ross Garfield <rossgarfield@gmail.com>
2025-07-11 16:04:55 -0500

    fix: explicitly set vite root to avoid vercel path resolution issue

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+console.log('Vite resolving alias:', path.resolve(__dirname, 'src/api'))
+
+
 export default defineConfig({
+  root: __dirname,
   plugins: [react()],
   resolve: {
     alias: {
